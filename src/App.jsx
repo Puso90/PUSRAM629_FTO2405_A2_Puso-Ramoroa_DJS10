00aa153197f1b1c258b5ddad955e031401c02952
Puso90 <juwishbrands@gmail.com>
2024-10-08 21:55:07 +0200

    Api shows on the console

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css'
+import { useState, useEffect } from "react"
 
 function App() {
 
+  const URL = 'https://jsonplaceholder.typicode.com/posts';
+
+  
+  const [posts, setPosts] = useState([])
+
+
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await fetch(URL)
+        result.json()
+          .then(json => {
+            console.log(json) 
+          })
+          
+      }
+      fetchData()
+    }, [])
+  
 
   return (
-    <>
-      <h1>
-        I will start!
-      </h1>
-    </>
+
+    <div className="App">
+      <h1>Posts</h1>
+      {posts}
+    </div>
   )
 }
 
