3f70e9ead46e4bd5c8750c803f011dde1773c26c
Puso90 <juwishbrands@gmail.com>
2024-10-09 13:28:47 +0200

    created BlogPosts components, Added BlogPosts inside App.jsx divs, Posts showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { useState, useEffect } from "react"
+import React from 'react';
+import BlogPosts from '../components/BlogPosts';
 
 function App() {
 
-  const URL = 'https://jsonplaceholder.typicode.com/posts';
-
-  
-  const [posts, setPosts] = useState([])
-
-
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await fetch(URL)
-        result.json()
-          .then(json => {
-            console.log(json) 
-          })
-          
-      }
-      fetchData()
-    }, [])
   
 
   return (
 
-    <div className="App">
-      <h1>Posts</h1>
-      {posts}
+    <div>
+      <BlogPosts />
     </div>
+    
   )
 }
 
